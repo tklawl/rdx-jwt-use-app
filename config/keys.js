c965845b235855a4b1e9642cb9291ca4aae2dece
tklawl <tny.ka@hotmail.com>
2020-08-18 10:29:30 +1000

    Quick fix

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,1 @@
-module.exports = {
-    mongoURI: "mongodb+srv://jj:tonykha@cluster0-yxjam.mongodb.net/test?retryWrites=true&w=majority",
-    secretOrKey: "secret"
-};
-//Remember to go to "Network Access" under SECURITY and add your IP address!
+...
